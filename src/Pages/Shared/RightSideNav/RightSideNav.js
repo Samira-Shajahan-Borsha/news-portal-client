3006f0e2b48938c85b0c2f8151bd28564d730408
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-02-28 15:02:44 +0600

    responsive navbar created

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { FaGoogle, FaGithub, FaFacebook, FaTwitter, FaTwitch, FaWhatsapp } from "react-icons/fa";
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right Side nav</h2>
+            <ButtonGroup vertical>
+                <Button variant="outline-primary" className='mb-2'><FaGoogle /> Log in Google </Button>
+                <Button variant="outline-dark"><FaGithub /> Log in with Github</Button>
+            </ButtonGroup>
+            <div className='mt-4'>
+                <h5>Find Us On: </h5>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook /> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp /> Whatsapp</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter /> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch /> Twich</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <div>
+                <BrandCarousel></BrandCarousel>
+            </div>
         </div>
     );
 };
